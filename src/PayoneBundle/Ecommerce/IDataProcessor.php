42f2ba3df8b8d3779fc4057ffc2e57c8310bec72
Fabian Pechstein <fabian.pechstein@asioso.com>
2019-03-29 15:51:57 +0100

    add copyright, move example DataProcessor

--- src/PayoneBundle/Ecommerce/IDataProcessor.php
+++ src/PayoneBundle/Ecommerce/IDataProcessor.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This source file is available under  GNU General Public License version 3 (GPLv3)
+ *
+ * Full copyright and license information is available in LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Asioso GmbH (https://www.asioso.com)
+ *
+ */
 
 namespace PayoneBundle\Ecommerce;
 
