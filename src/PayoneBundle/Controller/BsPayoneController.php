c033ed5fc3f2a286a00a4dd2c2d612aa149857d5
Fabian Pechstein <fabian.pechstein@asioso.com>
2020-04-24 16:15:11 +0200

    [bug-fix] use pimcore tool::getclientIp() to retrieve user's ip address

--- src/PayoneBundle/Controller/BsPayoneController.php
+++ src/PayoneBundle/Controller/BsPayoneController.php
@@ -199,7 +199,7 @@ class BsPayoneController extends AbstractCartAware
                 'bic' => $request->get('bic'),
                 'mandate_identification' => $request->get('mandate'),
                 'bankCountry' => $request->get('bankCountry'),
-                'clientIp' => empty($request->getClientIp()) ? $_SERVER['REMOTE_ADDR'] : $request->getClientIp(),
+                'clientIp' => \Pimcore\Tool::getClientIp($request),
             ];
 
             try{
